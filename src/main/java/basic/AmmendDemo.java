beafed3f4b8008468faff26adb0fa154ad0a98f0
Ramesh A <ramesharpu@gmail.com>
2025-02-14 08:44:09 +0530

    added amend demo class file

---
+++ src/main/java/basic/AmmendDemo.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class AmmendDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello There!!!");
+		System.out.println("Welcome to my team");
+
+	}
+
+}
